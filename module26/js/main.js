adff03e943de77c24f914650bb95f4335a2763cb
Ajla-bl <ajlablakqori1@gmail.com>
2026-05-18 00:07:20 +0200

    module26

---
+++ module26/js/main.js
@@ -0,0 +1,90 @@
+// console.log("Goodbye");
+
+// var x = 10;
+
+// var value = "Digital School";
+
+//1.   Numbers: 5, 6.5, 7, etc.
+
+//2.    String: “Hello GeeksforGeeks” etc.
+
+//3.    Boolean: Represent a logical entity and can have two values: true or false.
+
+//4.    Null: This type has only one value: null.
+
+//5.    Undefined: A variable that has not been assigned a value is undefined.
+
+//6.    Object: It is the most important data type and forms the building blocks for modern JavaScript.
+
+
+//The name must start with a letter (a to z or A to Z), underscore( _ ), or dollar( $ ) sign.
+
+// After the first letter we can use digits (0 to 9), for example, value1.
+
+//  JavaScript variables are case sensitive, for example, x and X are different variables. 
+
+
+// var a = 50+50;
+// console.log(a);
+
+// var b =20;
+// var c =30;
+
+// var d = b+c;
+
+// console.log(d)
+
+
+// var s =32;
+// var k =44;
+// var h = s*k;
+
+// console.log(h)
+
+
+// var a = 99;
+// a++;
+// console.log(a)
+
+// a--;
+// console.log(a)
+
+// var x = 15;
+// x+=5;
+// console.log(x)
+
+// var x = 5;
+// var y = 2;
+
+// console.log(x==y)
+
+// console.log(x<y)
+
+
+// var firstName="digital";
+// var lastName = "School";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName)
+
+var school ="Our School";
+console.log('hello from${school')
+
+
+// var age = 24;
+
+// console.log(age>=18 && age <=65);
+
+
+// var var1 = 5;
+// var var2 =6;
+
+// console.log(var1==5 || var2==5);
+
+
+var var3 =9;
+var var2=10;
+
+console.log(!(var3==var4))
+
+
+
